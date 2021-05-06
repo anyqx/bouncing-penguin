013d085f597e1f85c45f45712f2a35ed4bd7f7f8
anyqx <annieyxu@berkeley.edu>
2021-05-06 11:27:25 -0700

    index.js update

--- src/index.js
+++ src/index.js
@@ -1,77 +1,77 @@
-// import Game from "./game.js";
+import Game from "./game.js";
 import GameView from "./game_view.js";
 
-document.addEventListener("DOMContentLoaded",  () => {
+document.addEventListener("DOMContentLoaded", function () {
   
   const gameCanvas = document.getElementsByClassName("game-canvas")[0];
   gameCanvas.width = 1200;
   gameCanvas.height = 700;
   const ctx = gameCanvas.getContext("2d");
-  const scoreCanvas = document.getElementsByClassName("score-canvas")[0];
-  scoreCanvas.width = 75;
-  scoreCanvas.height = 700;
-  const scoreBar = scoreCanvas.getContext("2d");
-//   const wto = new Audio("assets/audio/")
+  const energyBar = document.getElementsByClassName("energy-bar")[0];
+  energyBar.width = 75;
+  energyBar.height = 700;
+  const eBar = energyBar.getContext("2d");
+  const bgMusic = new Audio("assets/audio/bg_music.mp3")
 
-//   const directions = document.getElementById("directions-modal");
-//   const directionsBtn = document.getElementById("directions-btn");
-//   const close = document.getElementById("close-modal");
-//   const mute = document.getElementById("mute-symbol");
+  const directions = document.getElementById("directions-modal");
+  const directionsBtn = document.getElementById("directions-btn");
+  const close = document.getElementById("close-modal");
+  const mute = document.getElementById("mute-symbol");
 
-//   directionsBtn.addEventListener("click", () => {
-//     if (directions.style.display !== "block") {
-//       directions.style.display = "block";
-//     } else {
-//       directions.style.display = "none";
-//     }
-//   });
+  directionsBtn.addEventListener("click", () => {
+    if (directions.style.display !== "block") {
+      directions.style.display = "block";
+    } else {
+      directions.style.display = "none";
+    }
+  });
 
-//   close.addEventListener("click", () => {
-//     directions.style.display = "none";
-//   });
+  close.addEventListener("click", () => {
+    directions.style.display = "none";
+  });
 
-//   document.getElementById("audio-btn").addEventListener("click", () => {
-//     if (wto.muted) {
-//       wto.muted = false;
-//       mute.innerHTML = "&#x1f50a;"
-//     } else {
-//       wto.muted = true;
-//       mute.innerHTML = "&#x1f507;"
-//     }
-//   });
+  document.getElementById("audio-btn").addEventListener("click", () => {
+    if (bgMusic.muted) {
+      bgMusic.muted = false;
+      mute.innerHTML = "&#x1f50a;"
+    } else {
+      bgMusic.muted = true;
+      mute.innerHTML = "&#x1f507;"
+    }
+  });
   
-//   document.getElementById("play-btn").addEventListener("click", () => {
+  document.getElementById("play-btn").addEventListener("click", () => {
 
-//     const endPositions = ["endPos1", "endPos2"]
-//     const endPos = endPositions[Math.floor(Math.random() * Math.floor(2))];
+    const endPositions = ["endPos1", "endPos2"]
+    const endPos = endPositions[Math.floor(Math.random() * Math.floor(2))];
     
-//     const game = new Game(ctx, eBar, endPos);
-//     new GameView(game, ctx, eBar).start();
+    const game = new Game(ctx, eBar, endPos);
+    new GameView(game, ctx, eBar).start();
 
-//     wto.currentTime = 0;
-//     wto.play();
+    bgMusic.currentTime = 0;
+    bgMusic.play();
 
-//     let moveLeft = false;
-//     let moveRight = false;
+    let moveLeft = false;
+    let moveRight = false;
 
-//     document.addEventListener("keydown", event => {
-//       if (event.code === "ArrowLeft") {
-//         moveLeft = true;
-//         game.moveBolt(moveLeft, moveRight);
-//       } else if (event.code === "ArrowRight") {
-//         moveRight = true;
-//         game.moveBolt(moveLeft, moveRight);
-//       }
-//     });
+    document.addEventListener("keydown", event => {
+      if (event.code === "ArrowLeft") {
+        moveLeft = true;
+        game. movePenguin(moveLeft, moveRight);
+      } else if (event.code === "ArrowRight") {
+        moveRight = true;
+        game. movePenguin(moveLeft, moveRight);
+      }
+    });
   
-//     document.addEventListener("keyup", event => {
-//       if (event.code === "ArrowLeft") {
-//         moveLeft = false;
-//         game.moveBolt(moveLeft, moveRight);
-//       } else if (event.code === "ArrowRight") {
-//         moveRight = false;
-//         game.moveBolt(moveLeft, moveRight);
-//       }
-//     });
-//   });
+    document.addEventListener("keyup", event => {
+      if (event.code === "ArrowLeft") {
+        moveLeft = false;
+        game. movePenguin(moveLeft, moveRight);
+      } else if (event.code === "ArrowRight") {
+        moveRight = false;
+        game. movePenguin(moveLeft, moveRight);
+      }
+    });
+  });
 });
