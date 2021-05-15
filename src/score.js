f6e7446b1a92359e5c79c1a22571ba97786f8848
anyqx <annieyxu@berkeley.edu>
2021-05-14 21:03:44 -0700

    direction edit

--- src/score.js
+++ src/score.js
@@ -10,7 +10,6 @@ class Score {
     this.score.font = '22px Itim cursive';
     this.score.fillStyle = 'rgb(255,107,107)';
     this.score.fillText("SCORE: "+this.currentScore, 50,20)
-    
   }
 
 }
